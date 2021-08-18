6bac5d621907d209b27094f71e64ec2e10e32a39
d0rianb <dorian.beauchesne@icloud.com>
2021-08-18 12:04:58 +0200

    Fix rolling up and down

--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,11 @@ fn get_last_input(input: &str) -> &str {
     lazy_static! {
         static ref PATH_REGEX: Regex = Regex::new(r"/(\w*)$").unwrap();
     }
-    let groups =  PATH_REGEX.captures(input).unwrap();
-    return groups.get(1).map_or("", |m| m.as_str());
+    let groups =  PATH_REGEX.captures(input);
+    return match groups {
+        None => "",
+        Some(groups) => { groups.get(1).map_or("", |m| m.as_str()) }
+    }
 }
 
 fn get_path_from_input(input: &str) -> String {
@@ -44,15 +47,14 @@ fn write_header(stdout: &mut termion::raw::RawTerminal<std::io::Stdout>) {
     write!(
         stdout,
         "{}{}{}",
-        termion::clear::All,
+        termion::clear::CurrentLine,
         termion::cursor::Goto(1, 1),
         HEADER
     ).unwrap();
     stdout.lock().flush().unwrap();
 }
 
-// fn get_guess(input: &String, subfolders: Vec<DirEntry>) -> String {
-fn get_guess(text: &String, offset: usize) -> String {
+fn get_guess(text: &String, offset: i8) -> String {
     let path = get_path_from_input(&text);
     if !is_valid_path(&path) { return String::new(); }
     let subfolders = get_subfolder(&path);
@@ -72,16 +74,28 @@ fn get_guess(text: &String, offset: usize) -> String {
         })
         .sorted()
         .collect::<Vec<String>>();
+    let nb_subfolders = sorted_subfolders.len();
+
+    if last_input == "" {
+        return String::from(&sorted_subfolders[offset as usize % nb_subfolders]);
+    }
+
     let mut guess = String::new();
-    // write!(stdout, "\n{:?}", sorted_subfolders).unwrap();
-    for name in sorted_subfolders {
-        if name.as_str().cmp(last_input).is_ge() {
+    for i in 0..nb_subfolders {
+        let name = &sorted_subfolders[i] as &str;
+        if name.cmp(last_input).is_ge() {
             let input_len = last_input.len();
-            guess = name[(input_len + offset % sorted_subfolders.len())..].to_string();
-            break;
+            guess = name[input_len..].to_string();
+            break
         }
     }
     return guess;
+}
+
+fn fill_guess(text: &mut String, offset: i8) {
+    let ref guess = get_guess(text, offset);
+    if guess == "" { return; };
+    text.push_str(&(guess.to_owned() + "/"));
 }
 
 fn main() {
@@ -91,8 +105,9 @@ fn main() {
         "The terminal is not TTY compatible"
     );
     let mut stdout = stdout().into_raw_mode().unwrap();
+    write!(stdout, "{}", termion::clear::All).unwrap();
     let mut text = String::new();
-    let mut offset: usize = 0;
+    let mut offset: i8 = 0;
     write_header(&mut stdout);
     for c in stdin.keys() {
         write_header(&mut stdout);
@@ -100,10 +115,9 @@ fn main() {
         match key {
             Key::Ctrl('c') => break,
             Key::Backspace => { text.pop(); }
-            Key::Char('\t') => { text.push_str(&(get_guess(&text, offset) + "/")) }
-            Key::Right => { text.push_str(&(get_guess(&text, offset) + "/")) }
+            Key::Char('\t') | Key::Right => { fill_guess(&mut text, offset) }
             Key::Down => { offset += 1 }
-            Key::Up => { offset = max(offset - 1, 0) }
+            Key::Up => { offset -= max(offset - 1, 0) }
             Key::Char(' ') => { text.push_str("\\ ") }
             _ => { if let Key::Char(k) = key { text.push(k) } }
         }

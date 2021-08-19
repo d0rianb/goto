14713da1d7b5dee0ac8b7fc40a4b4a7c0849ac10
d0rianb <dorian.beauchesne@icloud.com>
2021-08-19 11:00:44 +0200

    Fix path not working properly

--- src/main.rs
+++ src/main.rs
@@ -21,20 +21,33 @@ fn is_valid_path(input: &str) -> bool {
 
 fn get_last_input(input: &str) -> &str {
     lazy_static! {
-        static ref PATH_REGEX: Regex = Regex::new(r"/(\w*)$").unwrap();
+        static ref INPUT_REGEX: Regex = match Regex::new(r#"/([\w.\-\\\\ ]*)$"#) {
+            Ok(re) => re,
+            Err(err) => panic!("{}", err)
+        };
     }
-    let groups =  PATH_REGEX.captures(input);
+    let groups =  INPUT_REGEX.captures(input);
     return match groups {
         None => "",
         Some(groups) => { groups.get(1).map_or("", |m| m.as_str()) }
     }
 }
 
-fn get_path_from_input(input: &str) -> String {
-    return input.replace(get_last_input(input), "").into();
+fn get_path_from_input(input: &str) -> &str {
+    lazy_static! {
+        static ref INPUT_REGEX: Regex = match Regex::new(r"^.*/") {
+            Ok(re) => re,
+            Err(err) => panic!("{}", err)
+        };
+    }
+    let groups = INPUT_REGEX.captures(input);
+    return match groups {
+        None => "",
+        Some(groups) => { groups.get(0).map_or("", |m| m.as_str()) }
+    }
 }
 
-fn get_subfolder(input: &String) -> Vec<fs::DirEntry> {
+fn get_subfolder(input: &str) -> Vec<fs::DirEntry> {
     return fs::read_dir(input)
         .unwrap()
         .filter_map(Result::ok)
@@ -47,7 +60,7 @@ fn write_header(stdout: &mut termion::raw::RawTerminal<std::io::Stdout>) {
     write!(
         stdout,
         "{}{}{}",
-        termion::clear::CurrentLine,
+        termion::clear::All,
         termion::cursor::Goto(1, 1),
         HEADER
     ).unwrap();
@@ -57,7 +70,7 @@ fn write_header(stdout: &mut termion::raw::RawTerminal<std::io::Stdout>) {
 fn get_guess(text: &String, offset: i8) -> String {
     let path = get_path_from_input(&text);
     if !is_valid_path(&path) { return String::new(); }
-    let subfolders = get_subfolder(&path);
+    let subfolders = get_subfolder(path);
     if subfolders.len() == 0 { return String::new(); }
     let last_input = get_last_input(&text);
 
@@ -83,10 +96,12 @@ fn get_guess(text: &String, offset: i8) -> String {
     let mut guess = String::new();
     for i in 0..nb_subfolders {
         let name = &sorted_subfolders[i] as &str;
-        if name.cmp(last_input).is_ge() {
+        if name.cmp(last_input).is_ge() && name.starts_with(last_input) {
             let input_len = last_input.len();
-            guess = name[input_len..].to_string();
-            break
+            if input_len <= name.len() {
+                guess = name[input_len..].to_string();`
+                break
+            }
         }
     }
     return guess;
